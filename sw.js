251b8b475cf70b0802f276e0dd865f3a1fff4912
Renegade2k6 <steve.t1@gmx.com>
2026-07-03 00:10:03 +0100

    Daily auto-update 2026-07-03

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Cache version is updated on each deploy via deploy.bat
  */
 
-const CACHE_VERSION = "1782947402";
+const CACHE_VERSION = "1783033803";
 const CACHE_NAME = `lfuleaks-v${CACHE_VERSION}`;
 
 const urlsToCache = [
